cb78f5b0e3551141291c9bb33659935290220928
Davis Vincent-MKN847 <Vince.Davis@motorolasolutions.com>
2016-05-30 12:41:09 -0500

    Added All needed extensions add Updated Fastlane

---
+++ Watch Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  Watch Extension
+//
+//  Created by Vince on 5/30/16.
+//  Copyright © 2016 45 Bit Code. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
